11a2148481c000edf9d38e7c727d4a4929b80d16
Alan Somers <asomers@FreeBSD.org>
2024-08-04 09:42:22 -0600

    Import freebsd-libgeom-sys
    Copied without git history from https://github.com/asomers/gstat-rs

---
+++ lib/freebsd-libgeom-sys/src/lib.rs
@@ -0,0 +1,20 @@
+// vim: tw=80
+//! Rust FFI bindings for FreeBSD's libgeom library
+//!
+//! These are raw, `unsafe` FFI bindings.  Here be dragons!  You probably
+//! shouldn't use this crate directly.  Instead, you should use the
+//! [`freebsd-libgeom`](https://crates.io/crates/freebsd-libgeom) crate.
+#![cfg_attr(crossdocs, doc = "")]
+#![cfg_attr(crossdocs, doc = "These docs are just stubs!  Don't trust them.")]
+// bindgen generates some unconventional type names
+#![allow(non_camel_case_types)]
+#![allow(non_upper_case_globals)]
+#![allow(non_snake_case)]
+
+#[cfg(not(crossdocs))]
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(crossdocs)]
+mod fakes;
+#[cfg(crossdocs)]
+pub use fakes::*;
