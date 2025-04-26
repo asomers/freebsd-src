3f44aba3c68109b645875d2914c6f79a5c1a153d
Shawn Webb <shawn.webb@hardenedbsd.org>
2025-04-26 22:40:25 +0000

    HBSD: Initial proof-of-concept for building vendored Rust applications
    This commit introduces bsd.rust.mk, which provides the basic ability to
    optionally build base userland applications that have been written in
    Rust.
    
    This new addition to the build framework is still very much a
    work-in-progress. This commit only serves as a method of saving and
    sharing early work.
    
    In order to build Rust components, users must set a new
    OPTIONAL_TOOLCHAIN variable to "rust-cargo". Otherwise, Rust components
    will be skipped.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

---
+++ share/mk/bsd.rust.mk
@@ -0,0 +1,67 @@
+.include <bsd.init.mk>
+.include <bsd.compiler.mk>
+.include <bsd.linker.mk>
+
+.if !target(__<bsd.rust.mk>__)
+__<bsd.rust.mk>__:	.NOTMAIN
+
+all:
+clean:
+cleandir:
+depend:
+includes:
+install:
+installconfig:
+obj:
+
+.if defined(OPTIONAL_TOOLCHAIN) && ${OPTIONAL_TOOLCHAIN} == "rust-cargo"
+
+LOCALBASE?=		/usr/local
+
+CARGO?=			${LOCALBASE}/bin/cargo
+CARGO_TARGET_DIR?=	${OBJROOT}${TARGET}.${TARGET_ARCH}/rust-cargo
+.export CARGO_TARGET_DIR
+
+RUSTC?=			${LOCALBASE}/bin/rustc
+.export RUSTC
+
+CARGO_PROFILE?=		release
+
+.if defined(PROG)
+CARGO_INSTALL_FLAGS+=	--root ${DESTDIR}${BINDIR}/${PROG}
+.elif defined(LIB)
+.error Building Rust library crates not currently required/supported.
+#CARGO_INSTALL_FLAGS+=	--root ${DESTDIR}${LIBDIR}/${LIB}
+.else
+.error Can only build application crates
+.endif
+
+CARGO_FLAGS+=		--offline
+
+all:
+	env -C ${.CURDIR} ${CARGO} build ${CARGO_FLAGS} --profile ${CARGO_PROFILE}
+
+install:
+.if defined(PROG)
+	${INSTALL} \
+		-o ${BINOWN} \
+		-g ${BINGRP} \
+		-m ${BINMODE} \
+		${CARGO_TARGET_DIR}/${CARGO_PROFILE}/${PROG} \
+		${DESTDIR}${BINDIR}/${PROG}
+.endif
+
+clean:
+	env -C ${.CURDIR} ${CARGO} clean --profile ${CARGO_PROFILE}
+
+cleandir: clean
+
+.include <bsd.obj.mk>
+.include <bsd.incs.mk>
+.include <bsd.sys.mk>
+
+.endif
+
+includes:
+
+.endif
